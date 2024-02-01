4595b8697de9daf9740ee6460d73cd3f2c227d98
dschifel <danielschifelbain@gmail.com>
2024-02-01 14:45:08 -0300

    New: implemented getAddressByUserId method + injected address service.

--- src/main/java/com/danielks/headspaceprojectweb/HsWeb/controllers/AddressController.java
+++ src/main/java/com/danielks/headspaceprojectweb/HsWeb/controllers/AddressController.java
@@ -1,9 +1,33 @@
 package com.danielks.headspaceprojectweb.HsWeb.controllers;
 
+import com.danielks.headspaceprojectweb.HsWeb.models.AddressDTO;
+import com.danielks.headspaceprojectweb.HsWeb.services.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/address")
 public class AddressController {
+    @Autowired
+    private AddressService addressService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<AddressDTO> getAddressByUserId(@PathVariable UUID userId){
+        Optional<AddressDTO> address = addressService.getAddressByUserId(userId);
+
+        if(address.isPresent()){
+            return new ResponseEntity<>(address.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
