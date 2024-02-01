c74a1e5f5c8d87914c358d29f6fe14694b3c2171
dschifel <danielschifelbain@gmail.com>
2024-02-01 14:46:39 -0300

    New: injected address repository

--- src/main/java/com/danielks/headspaceprojectweb/HsWeb/services/AddressService.java
+++ src/main/java/com/danielks/headspaceprojectweb/HsWeb/services/AddressService.java
@@ -1,7 +1,18 @@
 package com.danielks.headspaceprojectweb.HsWeb.services;
 
+import com.danielks.headspaceprojectweb.HsWeb.models.AddressDTO;
+import com.danielks.headspaceprojectweb.HsWeb.repositories.AddressRepository;
+import com.danielks.headspaceprojectweb.HsWeb.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class AddressService {
+    @Autowired
+    private AddressRepository addressRepository;
+
+
 }
